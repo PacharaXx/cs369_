6ae4e86639b418270dc3ccd8a1482a1887a93758
PacharaX <pachara5701@gmail.com>
2024-05-30 04:18:54 +0700

    chore: Update next.config.mjs for API and file upload proxying

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,22 +1,25 @@
-// next.config.mjs
 const nextConfig = {
   output: "export", // Set the output directory for static site generation
   reactStrictMode: true, // Enable React strict mode
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint during production builds
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: "http://localhost:3001/api/:path*", // Proxy to Backend
-  //     },
-  //     {
-  //       source: '/:path*',
-  //       destination: '/',
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*", // Proxy to Backend
+      },
+      {
+        source: "/uploads/:path*", // Proxy uploads requests to the backend
+        destination: "http://localhost:3001/uploads/:path*",
+      },
+      {
+        source: "/:path*",
+        destination: "/", // General fallback route
+      },
+    ];
+  },
 };
 
 export default nextConfig;
